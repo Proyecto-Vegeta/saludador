b21e4704e2b89d24d49917862259c155da18bcc5
guillermo <guiyottemolina@gmail.com>
2022-06-14 20:18:43 -0300

    agregue ingrese nombre

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Ingrese nombre ");
+        Scanner scanner = new Scanner(System.in);
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre);
+
+
+
     }
 }
