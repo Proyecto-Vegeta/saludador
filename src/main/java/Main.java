45d4edddfdf7b6ddfdb1519f7cd89d862374e6a6
LeoTheEvil <iceninjashidomaru@gmail.com>
2022-06-14 21:04:14 -0300

    Calculador de factorial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ingrese nombre ");
+        System.out.println("Ingrese numero:");
         Scanner scanner = new Scanner(System.in);
-        String nombre = scanner.nextLine();
-        System.out.println("Hola " + nombre);
-
-
-
+        String numero = scanner.nextLine();
+        int factor = Integer.parseInt(numero);
+        int factorial = 1;
+        for (int i=2; i<=factor; i++) {
+            factorial = factorial*i;
+        }
+        System.out.println("El factorial de " + factor + " es " + factorial);
     }
 }
